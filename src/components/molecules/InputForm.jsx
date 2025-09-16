c23cc187f2a0059a9c168bb498800fb3efd80b58
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-09-16 11:13:48 +0700

    update form login & regist

--- src/components/molecules/InputForm.jsx
+++ src/components/molecules/InputForm.jsx
@@ -5,7 +5,7 @@ const InputForm = ({ labelName, labelPassword }) => {
     <>
       <div className="flex flex-col gap-3 w-full">
         <input className="px-3 py-2 font-normal rounded-md border border-[#C7A700] text-xs" type="text" placeholder={labelName} />
-        <input className="px-3 py-2 font-normal rounded-md border border-[#C7A700] text-xs" type="text" placeholder={labelPassword} />
+        <input className="px-3 py-2 font-normal rounded-md border border-[#C7A700] text-xs" type="password" placeholder={labelPassword} />
       </div>
     </>
   );
