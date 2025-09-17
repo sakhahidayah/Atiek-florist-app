2f427bf290378c240cd1c2bae0fef0b3813e31ae
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-09-17 09:56:39 +0700

    update placeholder

--- src/components/molecules/FormFormulir.jsx
+++ src/components/molecules/FormFormulir.jsx
@@ -8,27 +8,30 @@ const FormFormulir = () => {
         <label htmlFor="" className="font-semibold text-sm">
           Nama Pengirim
         </label>
-        <input className="px-2 py-1 w-full border rounded" type="text" placeholder="Sakha hidayah ramadhan" />
+        <input className="px-2 py-1 w-full border rounded" type="text" placeholder="Masukkan nama anda" />
         <label htmlFor="" className="font-semibold text-sm">
           Nama Perusahaan
         </label>
-        <input className="px-2 py-1 w-full border rounded" type="text" placeholder="PT. Bongkar Turret" />
+        <input className="px-2 py-1 w-full border rounded" type="text" placeholder="Masukkan nama perusahaan anda" />
         <label htmlFor="" className="font-semibold text-sm">
           Nama Yang Bersangkutan
         </label>
-        <input className="px-2 py-1 w-full border rounded" type="text" placeholder="Arthur Leywin" />
+        <input className="px-2 py-1 w-full border rounded" type="text" placeholder="Masukkan nama yang bersangkutan" />
         <label htmlFor="" className="font-semibold text-sm">
           Logo Perusahaan
         </label>
-        <input type="image" src={LogoNotFound} alt="" className="w-32 h-32 p-1 rounded border" />
+        <input type="image" src={LogoNotFound} alt="" className="w-32 h-32 p-1 rounded border" placeholder="Masukkan alamat pengiriman" />
         <label htmlFor="" className="font-semibold text-sm">
           Alamat Pengiriman
         </label>
-        <input className="px-2 py-1 w-full border rounded" type="text" placeholder="Jl.Batik Kumeli" />
+        <input className="px-2 py-1 w-full border rounded" type="text" placeholder="Masukkan alamat pengiriman" />
         <label htmlFor="" className="font-semibold text-sm">
           Provinsi
         </label>
         <select className="px-2 py-1 w-full border rounded" name="" id="">
+          <option value="" className="">
+            Select One
+          </option>
           <option value="">Jawa Tengah</option>
           <option value="">Jawa Barat</option>
           <option value="">Jawa Timur</option>
@@ -37,6 +40,8 @@ const FormFormulir = () => {
           Kabupaten
         </label>
         <select className="px-2 py-1 w-full border rounded" name="" id="">
+          <option value="">Select One</option>
+
           <option value="">Semarang</option>
           <option value="">Surabaya</option>
           <option value="">Bandung</option>
@@ -45,6 +50,8 @@ const FormFormulir = () => {
           Kecamatan
         </label>
         <select className="px-2 py-1 w-full border rounded" name="" id="">
+          <option value="">Select One</option>
+
           <option value="">Gajahmungkur</option>
           <option value="">Asemrowo</option>
           <option value="">Cibeunying</option>
@@ -53,6 +60,7 @@ const FormFormulir = () => {
           Metode Pembayaran
         </label>
         <select className="px-2 py-1 w-full border rounded" name="" id="">
+          <option value="">Select One</option>
           <option value="">Qris</option>
           <option value="">COD</option>
           <option value="">Transfer</option>
