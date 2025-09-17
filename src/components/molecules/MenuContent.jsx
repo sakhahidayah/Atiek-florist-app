f6ff2cc841f6e0c26bf2f915746181188c871924
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-09-17 13:09:06 +0700

    updated ( alt ) && ( HD IMAGE )

--- src/components/molecules/MenuContent.jsx
+++ src/components/molecules/MenuContent.jsx
@@ -16,22 +16,22 @@ const MenuContent = () => {
   return (
     <>
       <div onClick={handleCatalog} className="flex flex-col hover:bg-gray-100 cursor-pointer transition-all duration-300 rounded-lg gap-2 items-center justify-center p-3 shadow-md">
-        <img className="w-20 h-20 " src={Bouquet} alt="" />
+        <img className="w-20 h-20 " src={Bouquet} alt="flower" />
         <h5 className="font-bold text-lg text-center text-[#C7A700]">PILIH MODEL</h5>
         <p className="text-center text-sm font-light ">Pilih Model Yang Anda Suka</p>
       </div>
       <div onClick={handleFormulir} className="flex flex-col hover:bg-gray-100 cursor-pointer transition-all duration-300 rounded-lg gap-2 items-center justify-center p-3 shadow-md">
-        <img className="w-20 h-20 " src={FormulirImg} alt="" />
+        <img className="w-20 h-20 " src={FormulirImg} alt="formulir" />
         <h5 className="font-bold text-lg text-center text-[#C7A700]">ISI FORMULIR</h5>
         <p className="text-center text-sm font-light ">Isi Fromulir Pembelian</p>
       </div>
       <div className="flex flex-col hover:bg-gray-100 cursor-pointer transition-all duration-300 rounded-lg gap-2 items-center justify-center p-3 shadow-md">
-        <img className="w-20 h-20 " src={CashImg} alt="" />
+        <img className="w-20 h-20 " src={CashImg} alt="Cash" />
         <h5 className="font-bold text-lg text-center text-[#C7A700]">PEMBAYARAN</h5>
         <p className="text-center text-sm font-light ">Bayar Dengan Berbagai Metode</p>
       </div>
       <div onClick={handleLacak} className="flex flex-col hover:bg-gray-100 cursor-pointer transition-all duration-300 rounded-lg gap-2 items-center justify-center p-3 shadow-md">
-        <img className="w-20 h-20 " src={TruckImg} alt="" />
+        <img className="w-20 h-20 " src={TruckImg} alt="Delivery" />
         <h5 className="font-bold text-lg text-center text-[#C7A700]">TUNGGU PESANAN</h5>
         <p className="text-center text-sm font-light ">Tunggu Pesanan Dirumah</p>
       </div>
--- src/components/organism/ContentFormulir.jsx
+++ src/components/organism/ContentFormulir.jsx
@@ -8,7 +8,7 @@ const ContentFormulir = () => {
   return (
     <>
       <div className="w-full h-[200px] md:h-[300px] relative bg-center bg-cover flex flex-col justify-top  items-center" style={{ backgroundImage: `url(${FormulirImg})` }}>
-        <div className="bg-black/65 inset-0 absolute"></div>
+        <div className="bg-black/55 inset-0 absolute"></div>
         <div className="flex flex-col gap-1 z-10 items-center">
           <HeaderFormulir />
           <div className="flex flex-col gap-5 justify-center items-center w-[300px] md:w-[600px] md:py-10 md:px-7 h-max rounded bg-white shadow-lg p-5 mb-20">
